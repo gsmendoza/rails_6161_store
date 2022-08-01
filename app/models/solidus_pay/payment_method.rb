96573210cd589553bde590f2b09a4b18b6428b1b
George Mendoza <gsmendoza@gmail.com>
2022-08-01 18:26:44 +0800

    Integrate SolidusPay::PaymentMethod with Transaction

--- app/models/solidus_pay/payment_method.rb
+++ app/models/solidus_pay/payment_method.rb
@@ -1,2 +1,17 @@
 class SolidusPay::PaymentMethod < Spree::PaymentMethod
+  def payment_source_class
+    SolidusPay::Transaction
+  end
+
+  def supports?(source)
+    source.is_a?(payment_source_class)
+  end
+
+  def reusable_sources(order)
+    return [] unless order.user
+
+    order.user.wallet_payment_sources.map(&:payment_source).select do |source|
+      supports?(source) && source.reusable?
+    end
+  end
 end
